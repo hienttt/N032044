204caaf5eaf48b50853d98559bdd3c1299e4a0fb
hienttt <45008927+hienttt@users.noreply.github.com>
2018-12-10 00:23:04 +0700

    Update xulydangky.php

--- xulydangky.php
+++ xulydangky.php
@@ -1,13 +1,6 @@
 <?php
 //kết nối kiểm tra
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "login";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+
 if($_POST["txtname"]==NULL)
 {
     echo"Xin vuii lòng nhập lại username";
@@ -33,13 +26,32 @@ else
 }
 
 
-$sql = "INSERT INTO users (name , pass,email)
-VALUES ('$username','$password','$email')";
+if($ten && $mk && $mail)
+{
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sqli_checkuser = "SELECT * FROM users WHERE name='$ten' AND email='$mail'";
+$resultcheck = mysqli_query($conn,$sqli_checkuser);
+$check=mysqli_fetch_row($resultcheck);
+if($check)
+    {
+    echo"Tài khoản đã tồn tại vui lòng đăng ký lại</br> <a href='dangky.php'>Đăng ký</a>";
+    }
+
+else{
+    $sql = "INSERT INTO users (name , pass,email) VALUES ('$ten','$mk','$mail')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
+}
+    }
 $conn->close();
 ?>
