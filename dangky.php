1b08700d71be95260ad4293808e603b499542fbd
hienttt <45008927+hienttt@users.noreply.github.com>
2018-12-18 14:09:55 +0700

    Update dangky.php

--- dangky.php
+++ dangky.php
@@ -1,37 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>register form</title>
-</head>
-<body>
-    <form action="xuly.php" method="POST">
+<?php
+require('catgheplayout/header.php');
+$loi=array();
+$user=$pass=$mail=NULL;
+$loi["username"]=NULL;
+$loi["password"]=NULL;
+$loi["email"]=$loi["register"]=NULL;
+
+if(isset($_POST['ok']))
+{
+    if(empty($_POST['txtn']))
+    {
+        $loi["username"]="* Xin vui lòng nhập lại username</br>";
+    }
+    else{
+        $user=$_POST['txtn'];
+    }
+    if(empty($_POST['txtp']))
+    {
+        $loi["password"]="* Xin vui lòng nhập lại mật khẩu</br>";
+    }
+    else
+    {
+        $pass=md5($_POST['txtp']);
+    }
+    if(empty($_POST['txtmail']))
+    {
+        $loi["email"]="* Xin vui lòng nhập lại mail</br>";
+    }
+    else
+    {
+        $mail=$_POST['txtmail'];
+    }
+     if($user && $pass &&  $mail)
+    {
+        $conn =  mysqli_connect("localhost","root","","login");
+        if(!$conn){
+            die('Ket noi that bai'.mysqli_connect_error());
+        }
+        $sqli_checkuser = "SELECT * FROM users WHERE name='$user' AND email='$mail'";
+        $resultcheck = mysqli_query($conn,$sqli_checkuser);
+        $check=mysqli_fetch_row($resultcheck);
+        if($check)
+            {
+            $loi["register"]="Tài khoản đã tồn tại vui lòng đăng ký lại</br>";
+            }
+
+        else
+        {
+            $sql = "INSERT INTO users (name , pass,email,level) VALUES ('$user','$pass','$mail','1')";
+         if (mysqli_query($conn, $sql))
+                //Thông báo nếu thành công
+                $loi["register"]='Đăng ký thành công';
+            else
+                //Hiện thông báo khi không thành công
+                $loi["register"]= 'Không thành công. Lỗi' . mysqli_error($conn);
+            //ngắt kết nối
+            mysqli_close($conn);
+        }
+    }
+}
+?>
+<fieldset style="width:300px;margin:50px auto 0px;height:100px">
+    <form action="dangky.php" method="POST">
         <table>
             <tr>
-                <td> User name: </td>
-                <td> <input type="text" name="txtname"></td>
+                <td> Họ và tên: </td>
+                <td> <input type="text" name="txtn"></td>
             </tr>
             <tr>
-                <td>Password:</td>
-                <td><input type="password" name="txtpass"></td>
-            </tr>
-            <tr>
-                <td>Số điện thoại:</td>
-                <td><input type="text" name="txtP"></td>
+                <td> Mật khẩu:</td>
+                <td><input type="password" name="txtp"></td>
             </tr>
             <tr>
             <td>Email:</td>
-            <td><input type="text" name="txte"></td>
+            <td><input type="text" name="txtmail"></td>
             </tr>
             <tr>
-            <td> Giới tính:</td>
-            <td>  </td>
+                <td></td>
+                <td><input type="submit" name="ok" value="Đăng ký"></td>
             </tr>
         </table>
-        <input type="submit" name="submit" value="Create acount">
     </form>
-    
-</body>
-</html>
+ </fieldset>  
+ <div style="width:300px;margin:10px auto;height:150px;text-align:center;corlor:#F00">
+<?php
+    echo $loi["username"];
+    echo $loi["password"];
+    echo $loi["email"];
+    echo $loi["register"];
+?>
+ </div> 
+    <?php
+require('catgheplayout/buttom.php');
+?>
