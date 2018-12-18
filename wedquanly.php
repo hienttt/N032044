f424d20979be080679a0895c858b0187b55919cf
hienttt <45008927+hienttt@users.noreply.github.com>
2018-12-19 00:59:15 +0700

    Update wedquanly.php

--- wedquanly.php
+++ wedquanly.php
@@ -1,9 +1,22 @@
-<?php
-if($_SESSION["level"]==2){
-    echo"Trang quản trị của admin,<a href='admin.php'>(Đăng xuất)</a>";
-}
-else{
-    header("location:btlon.php");
-    exit();
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trang quản lý</title>
+    <link rel="stylesheet" type="text/css" href="admin.css">
+</head>
+<body>
+    <div id="top">
+        <h3> Chào mừng admin <a href="../btlon.php">Đăng xuất</a></h3>
+    </div>
+    <div id="menu">
+        <ul>
+        <li><a href="quanlybaiviet.php" > Quản lý bài viết</a></li>
+        <li><a href="quanlytaikhoan.php"> Quản lý tài khoản</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
