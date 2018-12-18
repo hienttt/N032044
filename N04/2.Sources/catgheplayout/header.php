bce9a749e5e56b0a9bbcd72ad599a5ef5bde9dee
= <=>
2018-12-18 15:06:31 +0700

    commit

---
+++ N04/2.Sources/catgheplayout/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tìm kiếm việc làm</title>
+    <link rel="stylesheet" href="catgheplayout/btlon.css" type="text/css">
+    <script type="text/javascript" src="btlon.js"></script>
+    <script src="btlon/jquery-3.3.1.min.js"></script>
+    <script>
+    $(document).ready(function())</script>
+</head>
+<body>
+    <div id="top">
+        <img src="anhbt/logo.jpg" alt="logo"  height="200px" width="800px">
+    </div>
+    <div id="menu">
+        <ul>
+            <li style="border-left:none"><a href="btlon.php">Trang chủ</a></li>
+            <li><a href="tuyendung.php">Tuyển dụng</a></li>
+            <li><a href="congty.php">Công ty</a></li>
+            <li><a href="dangky.php">Đăng ký</a></li>
+            <li><a href="dangnhap.php">Đăng nhập</a></li>
+        </ul>
+    </div>
+    
