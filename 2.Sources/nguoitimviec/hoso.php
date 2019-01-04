4b77f2d721cabd4407b71b774f09062cca29dfe1
BuiThiThuPhuong <44947708+BuiThiThuPhuong@users.noreply.github.com>
2019-01-04 10:50:56 +0700

    Update hoso.php

--- 2.Sources/nguoitimviec/hoso.php
+++ 2.Sources/nguoitimviec/hoso.php
@@ -87,12 +87,12 @@
 
         if( $hoten && $gioitinh  && $email )
         {
-                $conn =  mysqli_connect("localhost","root","","btlon");
+                $conn =  mysqli_connect("localhost","root","","vieclam");
                 if(!$conn){
                     die('Ket noi that bai'.mysqli_connect_error());
                 }
                 mysqli_set_charset($conn,"utf8");
-                $sql=("insert into cv(id,hoten,diachi,sodienthoai,email,gioitinh,ngaysinh,hocvan,kinhnghiem,ngonngu,thongtinkhac) values ('1',$hoten','$diachi','$sodienthoai','$email','$gioitinh','$ngaysinh','$hocvan','$kinhnghiem','$thongtinkhac')");
+                $sql=("insert into cv(hoten,diachi,sodienthoai,email,gioitinh,ngaysinh,hocvan,kinhnghiem,ngonngu,thongtinkhac) values ('$hoten','$diachi','$sodienthoai','$email','$gioitinh','$ngaysinh','$hocvan','$kinhnghiem','$ngonngu','$thongtinkhac')");
                 if (mysqli_query($conn, $sql))
                         //Thông báo nếu thành công
                         $loi["kq"]='Tạo  thành công';
@@ -111,56 +111,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Trang quản lý</title>
-    <link rel="stylesheet" type="text/css" href="admin/admin.css">
+    <title>Tìm kiếm việc làm</title>
+    <link rel="stylesheet" href="nguoixinviec.css" type="text/css">
+    <script src="timviec/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="btlon.js"></script>
+    <script>
+    $(document).ready(function())</script>
 </head>
 <body>
     <div id="top">
-        <h3> Chào mừng thành viên <a href="../btlon.php">Đăng xuất</a></h3>
+        <a href="btlon.php"><img src="../catgheplayout/tệp ảnh/logo.jpg" alt="logo"  height="200px" width="800px"></a>
     </div>
-    <div id="menu">
-        <ul>
-        <li><a href="quanlybaiviet.php" > Quản lý bài viết</a></li>
-        <li><a href="quanlytaikhoan.php"> Quản lý tài khoản</a></li>
-        <li><a href="quanlycongty.php"> Quản lý công ty</a></li>
+    <div id= "wrapper">
+        <ul >            
+          <li><a href="btlon1.php">Trang chủ</a></li>
+            <li><a href="hosoxinviec.php">Hồ sơ xin việc của tôi </a>
+            </li>
+            <li><a href="thuxinviec.php">Thư xin việc của tôi</a>
+            </li>
+            <li><a href="../btlon.php ">Đăng xuất</a></li>
         </ul>
     </div>
-    <div id="contain">
-    <fieldset style="wight:600px;margin:40px auto 10px;">
+    <div class="contain">
+    <fieldset style="width: :1000px;margin:40px auto 10px;height: 786px;">
         <legend> Hồ sơ xin việc</legend>
         <p  class="pull-right" style="text-align: right;"> <br>
         Những mục có dấu sao<strong class="text-danger">*</strong>  là bắt buộc.    </p>
         <form action="hoso.php" method="POST" >
-            <table cellspacing="15px">
+            <table cellspacing="15px" style="width: 100%">
                 <tr>
                 <td>   <h4>Thông tin cá nhân</h4> </td>
                 <td></td>
                 </tr>
                 <tr>
-                    <td>*Họ tên:</td>
+                    <td><strong class="text-danger">*</strong>Họ tên:</td>
                     <td><input type="text" size="70" name="txtht"></td>                
                 </tr>
                 <tr>
-                    <td>*Giới tính</td>
+                    <td><strong class="text-danger">*</strong>Giới tính</td>
                     <td>
                     <input type="radio" name="gt" value="Nam">Nam 
                     <input type="radio" name="gt" value="Nữ">Nữ
                     </td>                
                 </tr>
                 <tr>
-                    <td>*Ngày sinh:</td>
+                    <td><strong class="text-danger">*</strong>Ngày sinh:</td>
                     <td><input type="date"  name="txtns"></td>
                 </tr>
                 <tr>
-                    <td>*Học vấn:</td>
+                    <td><strong class="text-danger">*</strong>Học vấn:</td>
                     <td><textarea cols="60" rows="5" name="txthv"></textarea></td>
                 </tr>
                 <tr>
-                    <td>*Kinh nghiệm:</td>
-                    <td><textarea cols="60" rows="5" name="txtkn"></textarea></tdd>
+                    <td>Kinh nghiệm:</td>
+                    <td><textarea cols="60" rows="5" name="txtkn"></textarea></td>
                 </tr>
                 <tr>
-                    <td>*Ngôn ngữ:</td>
+                    <td><strong class="text-danger">*</strong>Ngôn ngữ:</td>
                     <td><input type="text" size="70" name="txtnn"></td>
                 </tr>
                 <tr>
@@ -172,15 +179,15 @@
                 <td></td>
                 </tr>
                 <tr>
-                    <td>*Địa chỉ:</td>
+                    <td><strong class="text-danger">*</strong>Địa chỉ:</td>
                     <td><input type="text" size="70" name="txtdc"></td>    
                 </tr>
                 <tr>
-                    <td>*Số điện thoai:</td>
+                    <td><strong class="text-danger">*</strong>Số điện thoai:</td>
                     <td><input type="text" name="txtsdt"></td>
                 </tr>
                 <tr>
-                    <td>*Email:</td>
+                    <td><strong class="text-danger">*</strong>Email:</td>
                     <td><input type="email" size="70" name="txtmail"></td>
                 </tr>
                 <tr>
@@ -188,7 +195,10 @@
                     <td><input type="submit" value="Thêm" name="ok" size="50"></td>
                 </tr>
             </table>
-            <div style="color:red">
+        </form>
+</fieldset>
+    </div>
+    <div style="color:red;margin: auto;width: 250px;">
                 <?php
                     echo $loi["kq"];
                     echo $loi["hoten"];
@@ -201,8 +211,9 @@
                     echo $loi["sodienthoai"];
                 ?>
             </div> 
-        </form>
-
-    </div>
- </body>
- </html>   
+    <br>
+    <?php
+require('../catgheplayout/buttom.php');
+?>
+</body>
+</html>
